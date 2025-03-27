44865fbbcdc99e01033bab8aacbf931bb2ca6e55
João Miccuci <joaomicucci3@gmail.com>
2025-03-27 19:31:49 -0300

    menu-title add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        'path':'',
+        'component': HomeComponent
+    },
+    
+];
